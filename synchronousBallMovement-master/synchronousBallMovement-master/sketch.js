d0aaf599adb9c35ea291d691a545ccb4a6d09a5a
abhimanyu-beep <76608899+abhimanyu-beep@users.noreply.github.com>
2021-02-05 20:55:28 +0530

    Add files via upload

---
+++ synchronousBallMovement-master/synchronousBallMovement-master/sketch.js
@@ -0,0 +1,21 @@
+var canvas
+var gameState  =0
+var playerCount
+var database
+var form
+var game
+var player
+
+function  setup() {
+
+  database  = firebase.database()
+  canvas = createCanvas(400,400)
+  game  = new Game()
+  game.getState()
+  game.start()
+  
+}
+
+function draw(){
+  
+}
